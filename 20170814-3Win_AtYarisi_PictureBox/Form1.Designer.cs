904ebfa0d100cbecbfdeacef92d9d91d077e95be
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-28 20:48:27 +0300

    created at 14.08.2014 (dd.mm.yyyy)

---
+++ 20170814-3Win_AtYarisi_PictureBox/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace _20170814_3Win_AtYarisi_PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pcAt1 = new System.Windows.Forms.PictureBox();
+            this.pcAt2 = new System.Windows.Forms.PictureBox();
+            this.pcAt3 = new System.Windows.Forms.PictureBox();
+            this.pcAt4 = new System.Windows.Forms.PictureBox();
+            this.pcAt5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.yenidenBaşlaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yarışıBaşlatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkımızdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt5)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pcAt1
+            // 
+            this.pcAt1.Image = ((System.Drawing.Image)(resources.GetObject("pcAt1.Image")));
+            this.pcAt1.Location = new System.Drawing.Point(12, 51);
+            this.pcAt1.Name = "pcAt1";
+            this.pcAt1.Size = new System.Drawing.Size(175, 96);
+            this.pcAt1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcAt1.TabIndex = 0;
+            this.pcAt1.TabStop = false;
+            // 
+            // pcAt2
+            // 
+            this.pcAt2.Image = ((System.Drawing.Image)(resources.GetObject("pcAt2.Image")));
+            this.pcAt2.Location = new System.Drawing.Point(12, 153);
+            this.pcAt2.Name = "pcAt2";
+            this.pcAt2.Size = new System.Drawing.Size(175, 96);
+            this.pcAt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcAt2.TabIndex = 1;
+            this.pcAt2.TabStop = false;
+            // 
+            // pcAt3
+            // 
+            this.pcAt3.Image = ((System.Drawing.Image)(resources.GetObject("pcAt3.Image")));
+            this.pcAt3.Location = new System.Drawing.Point(12, 255);
+            this.pcAt3.Name = "pcAt3";
+            this.pcAt3.Size = new System.Drawing.Size(175, 96);
+            this.pcAt3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcAt3.TabIndex = 2;
+            this.pcAt3.TabStop = false;
+            // 
+            // pcAt4
+            // 
+            this.pcAt4.Image = ((System.Drawing.Image)(resources.GetObject("pcAt4.Image")));
+            this.pcAt4.Location = new System.Drawing.Point(12, 357);
+            this.pcAt4.Name = "pcAt4";
+            this.pcAt4.Size = new System.Drawing.Size(175, 96);
+            this.pcAt4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcAt4.TabIndex = 3;
+            this.pcAt4.TabStop = false;
+            // 
+            // pcAt5
+            // 
+            this.pcAt5.Image = ((System.Drawing.Image)(resources.GetObject("pcAt5.Image")));
+            this.pcAt5.Location = new System.Drawing.Point(12, 459);
+            this.pcAt5.Name = "pcAt5";
+            this.pcAt5.Size = new System.Drawing.Size(175, 96);
+            this.pcAt5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcAt5.TabIndex = 5;
+            this.pcAt5.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFinish.BackColor = System.Drawing.Color.Yellow;
+            this.lblFinish.Location = new System.Drawing.Point(1127, 51);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(23, 523);
+            this.lblFinish.TabIndex = 6;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yenidenBaşlaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(148, 26);
+            // 
+            // yenidenBaşlaToolStripMenuItem
+            // 
+            this.yenidenBaşlaToolStripMenuItem.Name = "yenidenBaşlaToolStripMenuItem";
+            this.yenidenBaşlaToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.yenidenBaşlaToolStripMenuItem.Text = "Yeniden Başla";
+            this.yenidenBaşlaToolStripMenuItem.Click += new System.EventHandler(this.yenidenBaşlaToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yarışıBaşlatToolStripMenuItem,
+            this.hakkımızdaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1171, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yarışıBaşlatToolStripMenuItem
+            // 
+            this.yarışıBaşlatToolStripMenuItem.Name = "yarışıBaşlatToolStripMenuItem";
+            this.yarışıBaşlatToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.yarışıBaşlatToolStripMenuItem.Text = "Yarışı Başlat";
+            this.yarışıBaşlatToolStripMenuItem.Click += new System.EventHandler(this.yarışıBaşlatToolStripMenuItem_Click);
+            // 
+            // hakkımızdaToolStripMenuItem
+            // 
+            this.hakkımızdaToolStripMenuItem.Name = "hakkımızdaToolStripMenuItem";
+            this.hakkımızdaToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.hakkımızdaToolStripMenuItem.Text = "Hakkımızda";
+            this.hakkımızdaToolStripMenuItem.Click += new System.EventHandler(this.hakkımızdaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1171, 595);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.pcAt5);
+            this.Controls.Add(this.pcAt4);
+            this.Controls.Add(this.pcAt3);
+            this.Controls.Add(this.pcAt2);
+            this.Controls.Add(this.pcAt1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcAt5)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcAt1;
+        private System.Windows.Forms.PictureBox pcAt2;
+        private System.Windows.Forms.PictureBox pcAt3;
+        private System.Windows.Forms.PictureBox pcAt4;
+        private System.Windows.Forms.PictureBox pcAt5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yenidenBaşlaToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yarışıBaşlatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkımızdaToolStripMenuItem;
+    }
+}
+
---
+++ 20170814-3Win_AtYarisi_PictureBox/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace _20170814_3Win_AtYarisi_PictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+             
+            AtDurumDegistir(false);
+
+            //lblFinish.AutoSize = false;
+            //lblFinish.BackColor = Color.Yellow;
+        }
+
+        /*
+         * Eğer ses çalmak istersek using System.Media; kütüphanesini eklememiz gerekiyor.
+         * kütüphane içerisindeki SoundPlayer classı kullanılarak çalınacak olan ses dosyası yolu verilir. sonrasında play metotu ile ses çalınır.
+         */
+
+        Random r = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pcAt1.Left += r.Next(1,10);
+            pcAt2.Left += r.Next(1, 10);
+            pcAt3.Left += r.Next(1, 10);
+            pcAt4.Left += r.Next(1, 10);
+            pcAt5.Left += r.Next(1, 10);
+
+            AtKontrol(pcAt1);
+            AtKontrol(pcAt2);
+            AtKontrol(pcAt3);
+            AtKontrol(pcAt4);
+            AtKontrol(pcAt5);
+        }
+
+       void AtKontrol(PictureBox at)
+        {
+            if (at.Right > lblFinish.Left)
+            {
+                timer1.Stop();
+                AtDurumDegistir(false);
+                at.Enabled = true;
+                ses.Stop();
+                MessageBox.Show(at.Name +" Kazandı !");
+            }
+        }
+
+        void AtDurumDegistir(bool durum)
+        {
+            pcAt1.Enabled = pcAt2.Enabled = pcAt3.Enabled = pcAt4.Enabled = pcAt5.Enabled = durum;
+        }
+
+        SoundPlayer ses = new SoundPlayer();
+        private void yarışıBaşlatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            ses.SoundLocation = "kosuses.wav";
+            ses.Play();
+            AtDurumDegistir(true);
+        }
+
+        private void yenidenBaşlaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ses.Stop();
+            timer1.Enabled = false;
+            AtDurumDegistir(false);
+            pcAt1.Left = pcAt2.Left = pcAt3.Left = pcAt4.Left = pcAt5.Left = 12;
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hakkimizda h = new Hakkimizda();
+            h.ShowDialog();
+        }
+    }
+}
